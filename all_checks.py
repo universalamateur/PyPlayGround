0e32e2d74b14e8cd18bbf817e27daea23c8d642e
Falko Sieverding <falko@universalamateur.net>
2021-06-19 12:58:30 +0300

    Refactor in all checks and added untracked files

--- all_checks.py
+++ all_checks.py
@@ -1,20 +1,60 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
+# main libraries
 import os
+import shutil
 import sys
-
+import socket
+import psutil
 
 def check_reboot():
-    """Returns true if a computer has a pending reboot"""
-    return os.path.exists("/run/reboot-required")
+    """Returns True if computer has a pending reboot"""
+    return os.path.exists("/run/reboot-require")
 
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise"""
+    du = shutil.disk_usage(disk)
+    # Calculate the % of free space
+    percent_free = 100 * du.free / du.total
+    # Calculare free gigabytes
+    gb_free = du.free / 2**30
+    if percent_free < min_percent or gb_free < min_gb:
+        return True
+    return False
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise"""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_no_network_connection():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+def check_cpu_constrained():
+    """Returns True if the CPU is having too much usage, False otherwise."""
+    return psutil.cpu_percent(1) > 75
 
 def main():
-    if check_reboot():
-        print("pending reboot.")
+    checks = [(check_reboot, 'Pending Reboot.'),
+              (check_root_full, 'Root partition full.'),
+              (check_no_network_connection, 'No working network connection')]
+
+    everything_ok = True
+
+    for ck, msg in checks:
+        if ck():
+            print(msg)
+            everything_ok = False
+
+    if not everything_ok:
         sys.exit(1)
-    print("everythin ok")
+
+    print("Everything ok.")
     sys.exit(0)
 
-if __name__ == "__main__":
-  main()
+if __name__ == '__main__':
+    main()
--- no_lock.py
+++ no_lock.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import pyautogui
 import time
 
 """This script will simulate a keypress and prevent Windows from locking
@@ -10,9 +9,7 @@ def no_lock(button):
     try:
         print ('Press CTRL+C to stop.')
         while True:
-            pyautogui.press(button)     # Key pressed
             time.sleep(2)
-            pyautogui.press(button)     # Key released
             time.sleep(3)
 
     except Exception as ex:
