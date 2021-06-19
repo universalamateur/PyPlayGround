bc5aaeda169e9a1f6d71ded789b8e137b00a969d
Falko Sieverding <falko@universalamateur.net>
2021-06-19 12:54:56 +0300

    Added all checks py

---
+++ all_checks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+def check_reboot():
+    """Returns true if a computer has a pending reboot"""
+    return os.path.exists("/run/reboot-required")
+
+
+def main():
+    if check_reboot():
+        print("pending reboot.")
+        sys.exit(1)
+    print("everythin ok")
+    sys.exit(0)
+
+if __name__ == "__main__":
+  main()
