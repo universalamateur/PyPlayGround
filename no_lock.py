41fd733d5a54a742aa9c48fbd3ac83f27759e3ca
Falko Sieverding <falko@universalamateur.net>
2021-06-19 12:58:30 +0300

    Refactor in all checks and added untracked files

--- no_lock.py
+++ no_lock.py
@@ -1,15 +1,12 @@
 ## This script will simulate a keypress and prevent Windows from locking
 
-import pyautogui
 import time
 
 def no_lock(button):
     try:
         print ('Press CTRL+C to stop.')
         while True:
-            pyautogui.press(button)     # Key pressed
             time.sleep(2)
-            pyautogui.press(button)     # Key released
             time.sleep(3)
 
     except Exception as ex:
