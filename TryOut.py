9e0f713f75209b9d131e5fa628df86d91e87537c
Falko Sieverding <Falko@universalamateur.net>
2021-07-19 16:58:33 +0300

    Added some data and 2 Test Endpoints to the Flask Api Test_micro

--- TryOut.py
+++ TryOut.py
@@ -1,4 +1,50 @@
 #!/usr/bin/python
 
-print(ord('°'))
-print(chr(176))
+#print(ord('°'))
+#print(chr(176))
+import random
+custom_excuse={}
+data_excuses = {
+	"Intros": [
+		"Sorry I can't come",
+		"Please forgive my absence",
+		"This is going to sound crazy but",
+		"Get this:",
+		"I can't go because",
+		"I know you're going to hate me but",
+		"I was minding my own business and boom!",
+		"I feel terrible but",
+		"I regretfully cannot attend,",
+		"This is going to sound like an excuse but"
+	],
+	"Scapegoat": [
+		"my nephew",
+		"the ghost of Hitler",
+		"the Pope",
+		"my ex",
+		"high school marching band",
+		"Dan Rather",
+		"a sad clown",
+		"the kid from Air Bud",
+		"a professional cricket team",
+		"my Tinder date"
+	],
+	"Delay": [
+		"just shit the bed",
+		"died in front of me",
+		"won't stop telling me knock knock jokes",
+		"is having a nervous breakdown",
+		"gave me syphilis",
+		"poured lemonade in my gas tank",
+		"stabbed me",
+		"found my box of human teeth",
+		"stole my bicycle",
+		"posted my nudes on Instagram"
+	]
+}
+name_custom = "Falko"
+#for i in range(len(data_excuses["Intros"])):
+#  print(data_excuses["Intros"][i])
+
+custom_excuse['excuse'] = f'Dear {name_custom}, {data_excuses["Intros"][random.randint(0, len(data_excuses["Intros"])-1)]} {data_excuses["Scapegoat"][random.randint(0, len(data_excuses["Scapegoat"])-1)]} {data_excuses["Delay"][random.randint(0, len(data_excuses["Delay"])-1)]}'
+print(custom_excuse)
--- test_micro/app/views.py
+++ test_micro/app/views.py
@@ -1,6 +1,47 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, jsonify
 import datetime
+import random
+
+# Data
+data_excuses = {
+	"Intros": [
+		"Sorry I can't come",
+		"Please forgive my absence",
+		"This is going to sound crazy but",
+		"Get this:",
+		"I can't go because",
+		"I know you're going to hate me but",
+		"I was minding my own business and boom!",
+		"I feel terrible but",
+		"I regretfully cannot attend,",
+		"This is going to sound like an excuse but"
+	],
+	"Scapegoat": [
+		"my nephew",
+		"the ghost of Hitler",
+		"the Pope",
+		"my ex",
+		"high school marching band",
+		"Dan Rather",
+		"a sad clown",
+		"the kid from Air Bud",
+		"a professional cricket team",
+		"my Tinder date"
+	],
+	"Delay": [
+		"just shit the bed",
+		"died in front of me",
+		"won't stop telling me knock knock jokes",
+		"is having a nervous breakdown",
+		"gave me syphilis",
+		"poured lemonade in my gas tank",
+		"stabbed me",
+		"found my box of human teeth",
+		"stole my bicycle",
+		"posted my nudes on Instagram"
+	]
+}
 
 @app.route('/', methods=["GET"])
 def homr():
@@ -10,3 +51,25 @@ def homr():
 @app.route('/template')
 def template():
     return render_template('home.html')
+
+# A route to return all of the available entries in data.
+@app.route('/api/v1/excuses/all', methods=['GET'])
+def api_all():
+    return jsonify(data_excuses)
+
+# A route to return one personified excuse
+@app.route('/api/v1/excuses/custom', methods=['GET'])
+def api_custom():
+    custom_excuse={}
+    # Check if a name was provided as part of the URL.
+    # If name is provided, assign it to a variable.
+    # If no name is provided, display an error in the browser.
+    if 'name' in request.args:
+        name_custom = str(request.args['name'])
+    else:
+        return "Error: No name provided. Please specify a name."
+    
+    # Create with the given name a custome excuse with random Intro - Scapegoat and Delay
+    custom_excuse['excuse'] = f'Dear {name_custom}, {data_excuses["Intros"][random.randint(0, len(data_excuses["Intros"])-1)]} {data_excuses["Scapegoat"][random.randint(0, len(data_excuses["Scapegoat"])-1)]} {data_excuses["Delay"][random.randint(0, len(data_excuses["Delay"])-1)]}'
+
+    return jsonify(custom_excuse)
