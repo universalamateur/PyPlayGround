3a7693e3cc49e7d6babf82b8524cff273286328c
Falko <16203265+universalamateur@users.noreply.github.com>
2021-12-31 12:47:12 +0200

    Adding the Python Sec Scripts for TryHackME

--- SecurityResearch/dirEnum.py
+++ SecurityResearch/dirEnum.py
@@ -1,13 +1,13 @@
 import requests 
 import sys 
 
-sub_list = open("wordlist.txt").read() 
+sub_list = open("dirlist.txt").read()
 directories = sub_list.splitlines()
 
-for dir in directories:
-    dir_enum = f"http://{sys.argv[1]}/{dir}.html" 
+for one_dir in directories:
+    dir_enum = f"http://{sys.argv[1]}/{one_dir}.html" 
     r = requests.get(dir_enum)
-    if r.status_code==404: 
+    if r.status_code==404:
         pass
     else:
         print("Valid directory:" ,dir_enum)
